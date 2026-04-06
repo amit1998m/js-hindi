18826d94b54ba36508d9c798f153e68cce1e0468
Amit Kumar Yadav <149291571+amit1998m@users.noreply.github.com>
2026-04-06 18:45:25 +0000

    done

---
+++ 01_Basics/05_datesinJS.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date (1998 , 3 , 4) // new date create karne ke liye use this code
+console.log(myCreatedDate.toDateString()); // toDateString me (day-month-date-year) show karega
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
