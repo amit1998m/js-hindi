83c13408baeaaa6b1e05e93c65ae40a7c9aa4c6f
Amit Kumar Yadav <149291571+amit1998m@users.noreply.github.com>
2026-04-06 06:22:29 +0000

    complet

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144432
+let accountEmail = "amitkumar1998m@gmail.com"
+var accountPassword = "12345"
+accountCity = "Allahabd"
+let accountState;
+
+
+accountEmail = "amit123@gmail.com"
+accountPassword ="12321"
+accountCity = "Mirjapur"
+
+
+/*
+Prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; /*treat all js code as newer version*/
+
+// alert("Hello") // we are using nodejs,not browser
+
+let name = "Amit"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => yes or no
+// null => standlone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "age");
