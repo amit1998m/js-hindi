869b74df5e99110dfd4990baf6b442565f8ab286
Amit Kumar Yadav <149291571+amit1998m@users.noreply.github.com>
2026-04-07 06:51:28 +0000

    done

---
+++ 04_cantrol_flow/01_one.js
@@ -0,0 +1,30 @@
+// const isUserloggedIn = true
+// if (isUserloggedIn) {
+//     console.log("Exicuted");
+// }
+
+
+// const temperature = 100
+// if (temperature < 50) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Exicuted");
+
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log('user power : ${power}');
+// }
+
+
+
+const userloggedIn = true
+const debitCard = true
+if (userloggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+
