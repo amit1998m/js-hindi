6cdc4ef0c3de8a26a24714b43f1000187ab2c2d2
Amit Kumar Yadav <149291571+amit1998m@users.noreply.github.com>
2026-04-08 07:29:22 +0000

    Done

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+// while
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index ${index}`);
+    index = index+3
+    
+}
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10) {
+    
+}
